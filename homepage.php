1f66826464ef5fd801a867f1601bfb5b2ca62c4a
Maxi Popp <143752747+maxl23@users.noreply.github.com>
2023-09-15 21:58:05 +0200

    v3

---
+++ homepage.php
@@ -0,0 +1,25 @@
+<link href="stylehomepage.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+.beg {
+    font-family: sans-serif;
+}
+.head {
+    font-family: sans-serif;
+}
+.head {
+    font-family: sans-serif;
+}
+.Windows {
+    font-family: sans-serif;
+}
+</style>
+<a href="Benutzereinstellungen.html" class="head">Benutzer-Einstellungen</a>
+<a href="index.html" class="head"><h1>Ausloggen</h1></a>
+
+<h1 class="beg">
+  <strong>
+      Hallo, herzlich wilkommen im Download-Center!
+  </strong>
+</h1>
+<h3 class="Windows">Windows</h3>
+</body>
